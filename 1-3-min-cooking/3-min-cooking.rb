a95a28b8badbd025e6a4b865200338e7cab70364
Cj-bc <cj-bc@y-modify.org>
2019-03-11 02:27:27 +0900

    Add: 1-3-min-cooking> copying original

---
+++ 1-3-min-cooking/3-min-cooking.rb
@@ -0,0 +1,121 @@
+# offical: bpm 120
+use_bpm 120
+
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :E4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :F4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :E4
+sleep 0.5
+play :G4
+sleep 0.5
+play :B4
+sleep 0.5
+play :A4
+sleep 0.5
+
+play :G4
+sleep 0.25
+play :Gb4
+sleep 0.25
+play :F4
+sleep 0.25
+play :E4
+sleep 0.25
+play :D4
+sleep 0.5
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :E4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :D4
+sleep 0.5
+play :G4
+play :B4
+sleep 0.5
+play :G4
+play :B4
+sleep 0.5
+play :G4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :G4
+play :A4
+sleep 0.5
+sleep 0.5
+play :Gb4
+play :D5
+sleep 0.5
+sleep 0.5
+
+play :D4
+play :G4
+sleep 0.5
+sleep 0.5
+sleep 0.5
+play :E5
+play :G5
+sleep 0.25
+play :E5
+play :G5
+sleep 0.25
+
+play :C5
+play :E5
+sleep 0.5
+play :E5
+play :G5
+sleep 0.5
+play :E5
+play :G5
+sleep 0.5
+play :E5
+play :G5
+sleep 0.25
+play :E5
+play :G5
+sleep 0.25
+
+play :D5
+play :F5
+sleep 0.5
+play :F5
+play :G5
+sleep 0.5
